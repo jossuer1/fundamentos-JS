169595c3eb14b4cbbd43a10fd76622865e85b219
Emily <emily.pazmino02@epn.edu.ec>
2026-04-21 23:35:20 -0500

    Creación de archivos

--- common.js
+++
@@ -1,7 +0,0 @@
-export const loginAdmin = (username, password) => {
-    if (!username && !password)
-        return console.log("No autorizado");
-    console.log("Bienvenido ${username}");
-}
-
-export const LIKES = 150;
